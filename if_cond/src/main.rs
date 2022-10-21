2f9903b82b0734ea98cc113050d6966a6cc9f48c
sirmaxford <sirmaxford@gmail.com>
2022-10-21 10:30:57 +0300

    if statement vs expression

--- if_cond/src/main.rs
+++ if_cond/src/main.rs
@@ -6,4 +6,11 @@ fn main() {
     if n <= 5 && n>0 {println!("Num is less than 5");}
     else if n <= 10 && n >5 {println!("Num is less than 10");}
     else {println!("Num is not within range");}
+
+    let n:i32 = 5;
+    println!("{}",
+        if n <= 5 && n>0 {"Num is less than 5"}
+        else if n <= 10 && n >5 {"Num is less than 10"}
+        else {"Num is not within range"}
+    )
 }
